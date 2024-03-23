1daef5240bc09a7bf3d1bfb822b3840156947729
William Dale <wd40bug@gmail.com>
2024-03-23 18:48:40 -0500

    changed nvim theme to light in termux

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,2 +1,13 @@
+require('onedark').setup {
+  style = 'darker'
+}
+
+require('lualine').setup {
+  options = {
+    theme = 'light'
+    -- ... your lualine config
+  }
+}
+
 vim.opt.termguicolors = true
 vim.cmd.colorscheme "onedark"
