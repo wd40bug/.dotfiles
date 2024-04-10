c988e12ac3161bf7d2caa3333c8e88d4d81c3418
William D <wd40bug@gmail.com>
2024-04-10 17:57:40 -0500

    Powershell with other device prompt

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh init pwsh --config $HOME/.dotfiles/.mytheme.toml | Invoke-Expression
+function Set-EnvVar {
+    $env:COLS=$(&{$H=get-host;$H.ui.rawui.WindowSize.Width;})
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global -Force
