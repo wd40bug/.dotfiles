c6ebc9cf84aa83fb6f81daadc302bbb827f0eda2
William Dale <wd40bug@gmail.com>
2024-05-18 14:03:51 +0200

    Added descriptions to rust keybinds and removed focusing window

--- .config/nvim/lua/lsp/rust.lua
+++ .config/nvim/lua/lsp/rust.lua
@@ -14,28 +14,28 @@ vim.g.rustaceanvim = {
 
       vim.keymap.set('n', '<leader>a', function()
         vim.cmd.RustLsp('codeAction')
-      end, { buffer = bufnr })
+      end, { buffer = bufnr, desc = "Code action" })
 
       vim.keymap.set('n', '<leader>e', function()
         vim.cmd.RustLsp('explainError')
-      end)
+      end, {buffer = bufnr, desc = "Explain error"})
 
       vim.keymap.set('n', '<f5>', function()
         vim.cmd.RustLsp('runnables')
-      end, { buffer = bufnr })
+      end, { buffer = bufnr, desc="Run" })
 
       vim.keymap.set('n', '<f6>', function()
         vim.cmd.RustLsp('debuggables')
-      end, { buffer = bufnr })
+      end, { buffer = bufnr, desc = "Debug" })
 
 
       vim.keymap.set('n', '<f7>', function()
         vim.cmd.RustLsp('testables')
-      end, { buffer = bufnr })
+      end, { buffer = bufnr, desc = "Test" })
 
       vim.keymap.set('n', '<leader>m', function()
         vim.cmd.RustLsp('expandMacro')
-      end, { buffer = bufnr })
+      end, { buffer = bufnr, desc = "Expand macro"})
 
 
       -- you can also put keymaps in here
