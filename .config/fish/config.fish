e57e674d8df4c3a7801a9f1b71253269e7feff40
William Dale <wd40bug@gmail.com>
2024-09-26 10:21:46 -0500

    fixed cargo being bratty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
   oh-my-posh init fish --config ~/.mytheme.toml | source
   function set_poshcontext
     set -gx COLS (tput cols)
@@ -8,10 +7,8 @@ if status is-interactive
 
 
   abbr --add cl clear
-  bass source /etc/profile
-  pyenv init - | source
-  thefuck --alias | source
+  TF_SHELL=fish thefuck --alias | source
 
-  source "$HOME/.cargo/env.fish"
+  # source "$HOME/.cargo/env.fish"
 end
 
