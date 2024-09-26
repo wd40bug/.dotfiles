1b9cd63c086a8bbbed6bf3e7b94ab8f223bf1218
William Dale <wd40bug@gmail.com>
2024-09-26 14:56:28 -0500

    added some clangd stuff and a cargo plugin need to import the plugin on the master branch

--- .config/nvim/lua/lsp/c.lua
+++ .config/nvim/lua/lsp/c.lua
@@ -1,3 +1,9 @@
 Lspconfig.clangd.setup({
   capabilities = Lsp_capabilities,
+  on_attach = function (client, bufnr)
+    vim.lsp.inlay_hint.enable(bufnr, true)
+    vim.keymap.set('n', '<leader>h', function ()
+      vim.cmd.ClangdSwitchSourceHeader()
+    end, {buffer=bufnr, desc="Switch to header file"})
+  end
 })
