6074a9f5c7d543ed141978bad6586f0b6c42c328
William Dale <wd40bug@gmail.com>
2025-06-21 09:10:41 -1000

    idk, forgot to commit for a while x2

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,13 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  oh-my-posh init fish --config ~/.mytheme.toml | source
-  function set_poshcontext
-    set -gx COLS (tput cols)
-  end
+    # Commands to run in interactive sessions can go here
+    oh-my-posh init fish --config ~/.mytheme.toml | source
+    function set_poshcontext
+        set -gx COLS (tput cols)
+    end
 
 
-  abbr --add cl clear
-  TF_SHELL=fish thefuck --alias | source
-
-  # source "$HOME/.cargo/env.fish"
+    abbr --add cl clear
+    TF_SHELL=fish thefuck --alias | source
+    bass source /etc/profile
+    pyenv init - | source
 end
-
