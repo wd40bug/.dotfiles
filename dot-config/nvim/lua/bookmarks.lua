c1717e38947a03b9e66909c25a0719d912d91b67
William Dale <wd40bug@gmail.com>
2026-04-02 09:03:52 -0500

    update bookmarks

--- dot-config/nvim/lua/bookmarks.lua
+++ dot-config/nvim/lua/bookmarks.lua
@@ -1,5 +1,7 @@
 local bookmarks = {
   ["N"] = "~/.dotfiles/dot-config/nvim/",
+  ["I"] = "~/coding/CraftingInterpreters/",
+  ["R"] = "~/college/25Fall/ECE5380/rtos_project/rtos/",
   --TODO: Rest of the bookmarks
   -- Remember to `git update-index --skip-worktree ~/.dotfiles/dot-config/nvim/lua/bookmarks.lua`
 }
